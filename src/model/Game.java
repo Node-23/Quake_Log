a2b807e4cf14b18ba258e32a6a4cf5ca3022cc5a
Node-23 <matheusvinicius38@live.com>
2021-04-15 20:12:35 -0300

    Classes Game e Player criadas

---
+++ src/model/Game.java
@@ -0,0 +1,67 @@
+package model;
+
+public class Game {
+    private int id;
+    private int total_kills;
+    private Player[] players;
+
+    public Game(int id, int total_kills, Player[] players) {
+        this.id = id;
+        this.total_kills = total_kills;
+        this.players = players;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getTotal_kills() {
+        return this.total_kills;
+    }
+
+    public void setTotal_kills(int total_kills) {
+        this.total_kills = total_kills;
+    }
+
+    public Player[] getPlayers() {
+        return this.players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
+
+    @Override
+    public String toString() {
+        return "game_" + getId() + ": { \n" +
+        "total_kills: " + getTotal_kills() + "; \n" +
+        "players: ["+listaDePlayers()+"\n"+
+        "kills: { \n"+ listaDeKills() + "\n}"
+        ;
+    }
+
+    private String listaDePlayers() {
+        String playersList = "";
+        for (int i = 0; i < players.length; i++) {
+            if (i == players.length - 1) {
+                playersList += "'" + players[i].getNick() + "']";
+            } else {
+                playersList += "'" + players[i].getNick() + "', ";
+            }
+        }
+        return playersList;
+    }
+
+    private String listaDeKills() {
+        String killList = "";
+        for (int i = 0; i < players.length; i++) {
+            if (i == players.length - 1) {
+                killList += "'" + players[i].getNick() + "': " + players[i].getKills() + "\n}";
+            } else {
+                killList += "'" + players[i].getNick() + "': " + players[i].getKills() + ",\n";
+            }
+        }
+        return killList;
+    }
+
+}
---
+++ src/model/Player.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Player {
+    private String nick;
+    private int kills;
+
+    public Player(String nick, int kills) {
+        this.nick = nick;
+        this.kills = kills;
+    }
+
+    public String getNick() {
+        return this.nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public int getKills() {
+        return this.kills;
+    }
+
+    public void setKills(int kills) {
+        this.kills = kills;
+    }
+
+
+}
