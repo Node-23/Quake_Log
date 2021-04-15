4db2c632803bab17b8997c209552d111350c3358
Node-23 <matheusvinicius38@live.com>
2021-04-15 19:25:36 -0300

    Classe LogReader criada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import model.LogReader;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String[] log = LogReader.reader("/home/matheus/Área de Trabalho/games.log");
+        for (int i = 0; i < log.length; i++) {
+            System.out.println(log[i]);
+        }
     }
+
 }
+
+// /home/matheus/Área de Trabalho/games.log
---
+++ src/model/LogReader.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class LogReader {
+    
+    public static String[] reader(String path) throws IOException{
+        File file = new File(path);
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        String logText = new String(bytes, "UTF-8");
+        String[] log = logText.split("\n");
+        return log;
+    }
+
+}
